1477a009263c6db3014e894b6e7b82caf6638468
gliese-backup <glieseb+1@proton.me>
2025-02-18 11:45:25 +0000

    Add ejs

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello There");
+const express = require("express");
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("homepage", { name: "Wakati" });
+});
+
+app.listen(3000, () => {
+  console.log("Server fired up 🔥");
+});
