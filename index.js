659452a3da3a7b8fafbc552a32e67bd0aef576da
gliese-backup <glieseb+1@proton.me>
2025-02-18 12:01:42 +0000

    Create login page

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ app.set("view engine", "ejs"); // Setting ejs for templates
 app.use(express.static("public")); // Adding public dir
 
 app.get("/", (req, res) => {
-  res.render("homepage", { name: "Wakati" });
+  res.render("homepage");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 app.listen(3000, () => {
